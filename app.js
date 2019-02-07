788feabec15b6dc0f07c4091d4487d38d9988b55
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-06 23:50:55 -0600

    Create loop to populate article tags with retrieved videos.

--- app.js
+++ app.js
@@ -18,6 +18,40 @@ $(document).ready(function() {
 		//sends a request
 		$.getJSON(URL, options, function(data){
 			console.log(data);
+			var id = data.items[0].snippet.resourceId.videoId;
+			mainVid(id);
+			resultsLoop(data);
+
 		})
+	}
+
+	function mainVid(id) {
+		//`` allows us to direct variables directly
+		$('#video').html(`
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			`);
+	}
+
+	function resultsLoop(data) {
+		//using an each loop
+		$.each(data.items, function(i, item) {
+			var thumb = item.snippet.thumbnails.medium.url;
+			var title = item.snippet.title;
+			var description = item.snippet.description.substring(0,100); //first 100 characters
+
+			//using .append, not .html, because we want to reflect each data item in the loop on the DOM
+			$('main').append(`
+				<article>
+					<img src="${thumb}" alt="" class="thumb">
+					<div class="details">
+						<h4>${title}</h4>
+						<p>${description}</p>
+					</div>
+				</article>
+				`)
+		})
+
+
+
 	}
 });
